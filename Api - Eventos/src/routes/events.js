ecda4d6eed09b6abd0a9f3ad8ca48f38d16009eb
juliancape <juliancape360@gmail.com>
2024-10-06 15:38:33 -0500

    Agregar metodos al events

--- Api - Eventos/src/routes/events.js
+++ Api - Eventos/src/routes/events.js
@@ -2,13 +2,52 @@ const express = require("express");
 const router = express.Router(); // Manejador de rutas de express
 const eventoSchema = require("../models/events");
 
-// Nuevo evento
+// Agregar un evento
 router.post("/events", (req, res) => {
     const evento = new eventoSchema(req.body);
     evento
-        .save() // Guardar el evento en la base de datos
-        .then((data) => res.json(data)) // Respuesta en caso de éxito, devuelve el evento creado
-        .catch((error) => res.status(500).json({ message: error })); // Respuesta en caso de error
+        .save() 
+        .then((data) => res.json(data))
+        .catch((error) => res.status(500).json({ message: error })); 
 });
+
+// Obtener todos los eventos
+router.get("/events", (req, res) => {
+    eventoSchema
+        .find()
+        .then((data) => res.json(data)) 
+        .catch((error) => res.status(500).json({ message: error }));
+});
+
+// Actualizar un evento por su ID
+router.put("/events/:id", (req, res) => {
+    const { id } = req.params; 
+    const updatedData = req.body;
+
+    eventoSchema
+        .findByIdAndUpdate(id, updatedData, { new: true })
+        .then((data) => {
+            if (!data) {
+                return res.status(404).json({ message: "Evento no encontrado" });
+            }
+            res.json(data);
+        })
+        .catch((error) => res.status(500).json({ message: error })); 
+});
+
+
+router.delete("/events/:id", (req, res) => {
+    const { id } = req.params; 
+    eventoSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            if (!data) {
+                return res.status(404).json({ message: "Evento no encontrado" });
+            }
+            res.json({ message: "Evento eliminado con éxito" });
+        })
+        .catch((error) => res.status(500).json({ message: error }));
+});
+
 
 module.exports = router;
